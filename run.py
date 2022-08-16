89c16641476fc817a9df992dce05388f7470aac3
Jessica Fransson <jessicafransson@outlook.com>
2022-08-16 14:00:07 +0000

    added choice for get random word and initialise game

--- run.py
+++ run.py
@@ -1,17 +1,40 @@
 import random
 from words import word_list
 
-print("Welcome to Hangman, let's play!")
-  """
-  Welcome message for the players to know what game this is
-  """
 
-""" 
-Get the words to uppercase for easier readability
-"""
+print("Welcome to Hangman, let's play!")
+    """
+    Welcome message for the players to know what game this is
+    """
+
 def get_word():
+  """ 
+  Get the words to uppercase for easier readability
+  """
   word = random.choice(word_list)
   return word.upper()
+
+def get_random_word():
+  """
+  Pick a random word from text file to be used in the game
+  """
+  random_word = random.choice(open("words.py", "r").read().split('\n'))
+  return random_word.upper()
+
+def initialise_game():
+  """
+  Options for player to select play game, or see game rules
+  """
+  print(" Press " + "1" + "to play game")
+  print(" Press " + "2" + "to view rules")
+  options = False
+  while not options:
+    settings = input("\n")
+    if settings == "1":
+      options = True
+
+
+
 
 HANGMANPICS = ['''
   +---+
