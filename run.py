05dc06411f3c379f3b7c7ccc12cc8606f3df5798
Jessica Fransson <jessicafransson@outlook.com>
2022-08-17 12:39:43 +0000

    changed the way of random words list

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
 import random
-from words import word_list
-
+"""from words import word_list"""
 
 print("Welcome to Hangman, let's play!")
+print("________________________________")
+
+wordDictionary = ["appartment", "house", "flowers", "diamonds", "python", "javascript", "singer", "music", "developer"]
+
+""" 
+Choose a random word
+"""
+randomWord = random.choice(wordDictionary)
+
+for x in randomWord:
+  print("_", end="")
 
 def get_word():
   """ 
@@ -18,28 +28,10 @@ def get_random_word():
   random_word = random.choice(open("words.py", "r").read().split('\n'))
   return random_word.upper()
 
-def initialise_game():
-  """
-  Options for player to select play game, or see game rules
-  """
-  print(" Press " + "1" + "to play game")
-  print(" Press " + "2" + "to view rules")
-  options = False
-  while not options:
-    settings = input("\n")
-    if settings == "1":
-      options = True
-      difficulty = "default"
-      return difficulty
-
-    elif settings == "2":
-      options = True
-      game_rules()
-
-    else:
-      print("Please select 1 or 2", "choice")
-
 def print_hangman(wrong):
+  """
+  Get the symbol of hangman displayed when wrong guesses are made
+  """
   if(wrong == 0):
     print("\n+---+")
     print("    |")
@@ -82,3 +74,17 @@ def print_hangman(wrong):
     print("/|\  |")
     print("/ \  |")
     print("    ===")
+
+def printWord(guessedLetters):
+  """ Display the guesses being made and counter
+  """
+  counter=0
+  rightLetters=0
+  for char in randomWord:
+    if(char in guessedLetters):
+      print(randomWord[counter], end=" ")
+      rightLetters+=1
+    else:
+      print(" ", end=" ")
+    counter+=1
+  return rightLetters
